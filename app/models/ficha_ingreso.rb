ef073d148a3abfe8c3c2ef208d32fb571cc0bbd8
miguel quiroz <migue.quirozm@gmail.com>
2015-11-24 20:56:44 -0300

    cambios
    ultimoscambios

--- app/models/ficha_ingreso.rb
+++ app/models/ficha_ingreso.rb
@@ -1,2 +1,3 @@
 class FichaIngreso < ActiveRecord::Base
+
 end
--- app/models/reo.rb
+++ app/models/reo.rb
@@ -1,3 +1,6 @@
 class Reo < ActiveRecord::Base
   belongs_to :recinto
+  has_many :ficha_recintos
+  has_many :ficha_recintos, :through => :ficha_recintos
+
 end
